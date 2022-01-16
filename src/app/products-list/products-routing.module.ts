4352ebcc29261e17172a4617d3323ed744533b0c
belmondoweb <belmondoweb@gmail.com>
2022-01-16 15:45:50 +0200

    route path

--- src/app/products-list/products-routing.module.ts
+++ src/app/products-list/products-routing.module.ts
@@ -5,7 +5,7 @@ import { ProductsComponent } from './products.component';
 
 const routes: Routes = [{ path: '', component: ProductsComponent },
   { path: 'product/add/:id', component: ProductComponent},
-{ path: 'product/edit/:id', component: ProductComponent},];
+   { path: 'product/edit/:id', component: ProductComponent},];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
